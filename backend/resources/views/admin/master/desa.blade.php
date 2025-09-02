01440e92d290018405455d4d4e54b1f912d5a73d
Septaro Travian Gadha <septaro21@gmail.com>
2025-09-02 15:08:38 +0700

    Menambahkan menu navigasi untuk Master Data di sidebar admin, termasuk link untuk Kabupaten, Kecamatan, dan Desa. Juga memperbarui rute untuk halaman master data di file routes/admin.php.

---
+++ backend/resources/views/admin/master/desa.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="px-6 py-4">
+    <h1 class="text-2xl font-semibold text-gray-900 mb-4">Master - Desa</h1>
+    <div class="bg-white shadow rounded p-6 text-gray-700">
+        Halaman placeholder untuk data Desa.
+    </div>
+@endsection
+
+
