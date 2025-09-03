826cfdddc60d4ebf2919073671d381bc4c319616
ditofatahillah <ditofatahillah1@gmail.com>
2025-09-03 11:25:46 +0700

    update file structure frontend section

--- frontend/src/public/layout/Layout.jsx
+++ frontend/src/public/layout/Layout.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
-import Dashboard from '../dashboard/Dashboard'
-import FloodWarningTicker from '../common/StockTicker'
+import Dashboard from '../pages/Dashboard'
+import FloodWarningTicker from '../components/FloodTicker'
 
 const Layout = ({ children }) => {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Header />
       <FloodWarningTicker />
       <main className="w-full p-6">
         {children || <Dashboard />}
