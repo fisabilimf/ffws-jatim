5a237fc786a936a725885f03f995750c21a361a0
ditofatahillah <ditofatahillah1@gmail.com>
2025-09-09 14:20:10 +0700

    fix file structure, ganti nama card.jsx jadi sidecard.jsx agar lebih jelas

--- frontend/src/public/pages/Dashboard.jsx
+++ frontend/src/public/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Card from '../components/Card'
+import SideCard from '../components/SideCard'
 import Button from '../components/Button'
 
 const Dashboard = () => {
