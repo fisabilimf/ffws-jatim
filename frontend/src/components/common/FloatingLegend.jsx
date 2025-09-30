e5af545deca5d4c5dfb4eb8447a177c2cd59d355
mukhamadzaky <genduttt890@gmail.com>
2025-09-30 15:41:13 +0700

    commit

--- frontend/src/components/common/FloatingLegend.jsx
+++ frontend/src/components/common/FloatingLegend.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FloatingLegend = () => {
   return (
-    <div className="fixed bottom-5 right-2 z-30   p-3 flex items-center space-x-4">
+    <div className="fixed bottom-5 right-2 z-20 p-3 flex items-center space-x-4">
       <div className="flex items-center space-x-3">
         <div className="flex items-center space-x-1">
           <div className="w-3 h-3 rounded-full bg-green-500"></div>
