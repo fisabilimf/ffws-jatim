94d4e6226ac5a1a5986068d80d23ecac5fcc9814
mukhamadzaky <genduttt890@gmail.com>
2025-10-01 11:01:49 +0700

    commit

--- frontend/src/components/devices/MapboxMap.jsx
+++ frontend/src/components/devices/MapboxMap.jsx
@@ -536,7 +536,7 @@ const MapboxMap = ({ tickerData, onStationSelect, onMapFocus }) => {
         <>
           <button
             onClick={toggleRiverLayer}
-            className="absolute bottom-28 left-4 z-10 px-4 py-2 rounded-lg shadow-lg transition-colors flex items-center bg-red-500 hover:bg-red-600 text-white"
+            className="absolute bottom-28 left-4 z-10 px-4 py-2 rounded-lg shadow-lg transition-colors flex items-center bg-red-500 hover:bg-red-600 text-black"
           >
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
@@ -548,8 +548,8 @@ const MapboxMap = ({ tickerData, onStationSelect, onMapFocus }) => {
             onClick={toggleWaterEffect}
             className={`absolute bottom-16 left-4 z-10 px-4 py-2 rounded-lg shadow-lg transition-colors flex items-center ${
               showWaterEffect 
-                ? 'bg-orange-500 hover:bg-orange-600 text-white' 
-                : 'bg-blue-500 hover:bg-blue-600 text-white'
+                ? 'bg-orange-500 hover:bg-orange-600 text-black' 
+                : 'bg-blue-500 hover:bg-blue-600 text-black'
             }`}
           >
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
